3e1fa4c1883a19ff1263a4d25137ee67ebdd588d
Vaishravana <vaishravana.s@gmail.com>
2022-03-10 23:51:44 +0530

    Update test2.py

--- test_dir/test2.py
+++ test_dir/test2.py
@@ -6,6 +6,6 @@ parser.add_argument('integers', metavar='N', type=int, nargs='+',
 parser.add_argument('--sum', dest='accumulate', action='store_const',
                     const=sum, default=max,
                     help='sum the integers (default: find the max)')
-
+print("Added a print")
 args = parser.parse_args()
 print(args.accumulate(args.integers))
