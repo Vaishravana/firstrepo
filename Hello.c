9c9aa79b572eea206639d7ee443d7974c3084f59
Vaishravana <vaishravana.s@gmail.com>
2022-03-17 14:49:09 +0530

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -3,6 +3,7 @@ int main(void)
 {
    printf ("Hello from your first program!\n");
    printf ("The push triggers jenkins run");
+   printf ("The push triggers jenkins run 2");
    printf ("The push triggers jenkins run");
    return 0;
 }
