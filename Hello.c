96947973a699e26089bdd527d2206281dcf350d0
Vaishravana <vaishravana.s@gmail.com>
2022-05-27 20:57:00 +0530

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -4,5 +4,7 @@ int main(void)
    printf ("Hello from your first program!\n");
    printf ("The push triggers jenkins run 2");
    printf ("The push triggers jenkins");
+      printf ("The push triggers jenkins");
+      printf ("The push triggers jenkins");
    return 0;
 }
